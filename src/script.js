4529bddd048171ab4f4320f748d10246d52ad9b1
Andrii Avramenko <andreitvmm228@gmail.com>
2026-03-04 18:59:27 +0000

    Projects render, Lenis package and projects section edges

--- src/script.js
+++ src/script.js
@@ -1,1 +1,31 @@
-console.log("banana")
+import Lenis from 'lenis'
+import 'lenis/dist/lenis.css'
+
+import projects from "./projects.json";
+
+const placeholderImage =
+  "https://hips.hearstapps.com/hmg-prod/images/ginger-maine-coon-kitten-running-on-lawn-in-royalty-free-image-1719608142.jpg";
+const projectsList = document.querySelector(".projects-list");
+
+const lenis = new Lenis({
+  autoRaf: true
+});
+
+projects.projects.forEach((project) => {
+  let markup = `<li class="projects-item">
+              <img src="${project.thumbnail == true ? project.thumbnail : placeholderImage}" alt="Project thumbnail" class="projects-thumbnail">
+              <p class="projects-name">${project.name}</p>
+              <p class="projects-time">${new Date(
+                project.lastUpdated,
+              ).toLocaleDateString("en-GB", {
+                day: "2-digit",
+                month: "numeric",
+                year: "numeric",
+              })}</p>
+              <ul class="projects-links">
+                <li class="projects-link"><a href="${project.activePage}" target="_blank">Website</a></li>
+                <li class="projects-link"><a href="${project.sourceCode}" target="_blank">Source code</a></li>
+              </ul>
+            </li>`;
+  projectsList.insertAdjacentHTML("beforeend", markup);
+});
